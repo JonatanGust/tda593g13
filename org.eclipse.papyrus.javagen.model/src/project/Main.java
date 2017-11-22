6e1f96af3536c0108ff1ec212fe337d2e31c315c
Isar Arason <gusarais@student.gu.se>
2017-11-22 15:32:54 +0100

    Position robots properly

--- org.eclipse.papyrus.javagen.model/src/project/Main.java
+++ org.eclipse.papyrus.javagen.model/src/project/Main.java
@@ -28,29 +28,29 @@ public class Main {
 		new VerticalBoundary(10.0f, -10.0f, 10.0f, e, c);
 
 
-		new HorizontalWall(5f, -5f, 5f, e, c);
-		new HorizontalWall(0f, -5f, -3f, e, c);
-		new HorizontalWall(0f, -2f, 2f, e, c);
-		new HorizontalWall(0f, 3f, 5f, e, c);
-		new HorizontalWall(-5f, -5f, 5f, e, c);
-
-		new VerticalWall(-5f, -5f, -3f, e, c);
-		new VerticalWall(-5f, -2f, 2f, e, c);
-		new VerticalWall(-5f, 3f, 5f, e, c);
+		new VerticalWall(5f, -5f, 5f, e, c);
 		new VerticalWall(0f, -5f, -3f, e, c);
 		new VerticalWall(0f, -2f, 2f, e, c);
 		new VerticalWall(0f, 3f, 5f, e, c);
-		new VerticalWall(5f, -5f, -3f, e, c);
-		new VerticalWall(5f, -2f, 2f, e, c);
-		new VerticalWall(5f, 3f, 5f, e, c);
+		new VerticalWall(-5f, -5f, 5f, e, c);
+		
+		new HorizontalWall(-5f, -5f, -3f, e, c);
+		new HorizontalWall(-5f, -2f, 2f, e, c);
+		new HorizontalWall(-5f, 3f, 5f, e, c);
+		new HorizontalWall(0f, -5f, -3f, e, c);
+		new HorizontalWall(0f, -2f, 2f, e, c);
+		new HorizontalWall(0f, 3f, 5f, e, c);
+		new HorizontalWall(5f, -5f, -3f, e, c);
+		new HorizontalWall(5f, -2f, 2f, e, c);
+		new HorizontalWall(5f, 3f, 5f, e, c);
 
 
 		Set<RobotAvatar> robots = new HashSet<>();
 
-		RobotAvatar robot1 = new RobotAvatar(new Point(0, 0), "Robot 1");
-		RobotAvatar robot2 = new RobotAvatar(new Point(1, 3), "Robot 2");
-		RobotAvatar robot3 = new RobotAvatar(new Point(1, 0), "Robot 3");
-		RobotAvatar robot4 = new RobotAvatar(new Point(2, 3), "Robot 4");
+		RobotAvatar robot1 = new RobotAvatar(new Point(7, -1.5), "Robot 1");
+		RobotAvatar robot2 = new RobotAvatar(new Point(7, -0.5), "Robot 2");
+		RobotAvatar robot3 = new RobotAvatar(new Point(7, 0.5), "Robot 3");
+		RobotAvatar robot4 = new RobotAvatar(new Point(7, 1.5), "Robot 4");
 
 		robots.add(robot1);
 		robots.add(robot2);
