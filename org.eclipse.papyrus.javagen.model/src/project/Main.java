96fb891248cd53be8795cc99ac00ef13c9363e01
JonatanGust <jongust@student.chalmers.se>
2017-12-07 14:48:32 +0100

    missions are now contained in methods

--- org.eclipse.papyrus.javagen.model/src/project/Main.java
+++ org.eclipse.papyrus.javagen.model/src/project/Main.java
@@ -21,9 +21,113 @@ import simbad.sim.VerticalWall;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-
+		mission2();
+	}
+	public static void mission1() {
 		EnvironmentDescription e = new EnvironmentDescription();
 
+		Color c = Color.GRAY;
+
+		new HorizontalBoundary(-10.0f, -10.0f, 10.0f, e, c);
+		new HorizontalBoundary(10.0f, -10.0f, 10.0f, e, c);
+		new VerticalBoundary(-10.0f, -10.0f, 10.0f, e, c);
+		new VerticalBoundary(10.0f, -10.0f, 10.0f, e, c);
+
+
+		new VerticalWall(5f, -5f, 5f, e, c);
+		new VerticalWall(0f, -5f, -3f, e, c);
+		new VerticalWall(0f, -2f, 2f, e, c);
+		new VerticalWall(0f, 3f, 5f, e, c);
+		new VerticalWall(-5f, -5f, 5f, e, c);
+		
+		new HorizontalWall(-5f, -5f, -3f, e, c);
+		new HorizontalWall(-5f, -2f, 2f, e, c);
+		new HorizontalWall(-5f, 3f, 5f, e, c);
+		new HorizontalWall(0f, -5f, -3f, e, c);
+		new HorizontalWall(0f, -2f, 2f, e, c);
+		new HorizontalWall(0f, 3f, 5f, e, c);
+		new HorizontalWall(5f, -5f, -3f, e, c);
+		new HorizontalWall(5f, -2f, 2f, e, c);
+		new HorizontalWall(5f, 3f, 5f, e, c);
+		
+		Area[] areas = new Area[4];
+		
+		areas[0] = new Area(
+				new Boundary(5, 5, new Position(0, 0)), 
+				new LocationController(new Point(2.5, 2.5), 2.75, e), "Office", false, 1
+			);
+		areas[1] = new Area(
+				new Boundary(5, 5, new Position(0, -5)), 
+				new LocationController(new Point(2.5, -2.5), 2.75, e), "Office", false, 1
+			);
+		areas[2] = new Area(
+				new Boundary(5, 5, new Position(-5, -5)), 
+				new LocationController(new Point(-2.5, -2.5), 2.75, e), "Office", false, 1
+			);
+		areas[3] = new Area(
+				new Boundary(5, 5, new Position(-5, 0)), 
+				new LocationController(new Point(-2.5, 2.5), 2.75, e), "Office", false, 1
+			);
+
+		Environment env = new Environment(areas);
+
+		ArrayList<Robot> robots = new ArrayList<>();
+
+		Robot robot1 = new Robot(new Position(-7.5, -2.5), "Robot 1");
+		Robot robot2 = new Robot(new Position(7.5, -2.5), "Robot 2");
+		Robot robot3 = new Robot(new Position(7.5, 2.5), "Robot 3");
+		Robot robot4 = new Robot(new Position(-7.5, 2.5), "Robot 4");
+
+		robots.add(robot1);
+		robots.add(robot2);
+		robots.add(robot3);
+		robots.add(robot4);
+
+		
+				
+		ArrayList<Rover> rovers = new ArrayList<Rover>(4);
+		
+		for (Robot r : robots) {
+			rovers.add(new Rover(r, env, SimplestStrategy.getInstance()));
+		}
+		RoverLord roverLord = new RoverLord(env, rovers.toArray( new Rover[4] ));
+		
+		RoverView roverView = new RoverView(roverLord, roverLord);
+		
+		
+		AbstractSimulatorMonitor controller = new SimulatorMonitor(new HashSet<Robot>(robots), e, roverLord);
+		
+		ArrayList<ClassDiagram.Types.Point> misP1 = new ArrayList<ClassDiagram.Types.Point>();
+		misP1.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(-2.5, -2.5)));
+		misP1.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(2.5, -2.5)));
+		misP1.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(7.5, -2.5)));
+		
+		ArrayList<ClassDiagram.Types.Point> misP2 = new ArrayList<ClassDiagram.Types.Point>();
+		misP2.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(2.5, -2.5)));
+		misP2.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(2.5, 2.5)));
+		misP2.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(7.5, 2.5)));
+		
+		
+		ArrayList<ClassDiagram.Types.Point> misP3 = new ArrayList<ClassDiagram.Types.Point>();
+		misP3.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(2.5, 2.5)));
+		misP3.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(-2.5, 2.5)));
+		misP3.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(-7.5, 2.5)));
+		
+		ArrayList<ClassDiagram.Types.Point> misP4 = new ArrayList<ClassDiagram.Types.Point>();
+		misP4.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(-2.5, 2.5)));
+		misP4.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(-2.5, -2.5)));
+		misP4.add(new ClassDiagram.Types.Point(new ClassDiagram.Types.Position(-7.5, -2.5)));
+		
+		rovers.get(0).changeMission(new Mission(misP1));
+		rovers.get(1).changeMission(new Mission(misP2));
+		rovers.get(2).changeMission(new Mission(misP3));
+		rovers.get(3).changeMission(new Mission(misP4));
+	}
+
+	public static void mission2() {
+
+		EnvironmentDescription e = new EnvironmentDescription();
+		
 		Color blue = Color.BLUE;
 		Color red = Color.RED;
 		Color gray = Color.GRAY;
@@ -141,5 +245,4 @@ public class Main {
 		rovers.get(2).changeMission(new Mission(misP3));
 		rovers.get(3).changeMission(new Mission(misP4));
 	}
-
 }
