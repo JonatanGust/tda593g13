3e044de060d307aaf31beecc4579db37e01ff660
JonatanGust <jongust@student.chalmers.se>
2017-12-08 11:45:48 +0100

    fixed SM to use 3-way-roomcontroller

--- se.chalmers.cse.mdsd1718.yakindu/src/se/chalmers/cse/mdsd1718/yakindu/Assignment4Rooms.java
+++ se.chalmers.cse.mdsd1718.yakindu/src/se/chalmers/cse/mdsd1718/yakindu/Assignment4Rooms.java
@@ -1,5 +1,7 @@
 package se.chalmers.cse.mdsd1718.yakindu;
 
+import java.util.ArrayList;
+
 // This class implements an array of Area Controllers.
 // Trying to acquire access to area covered by controller
 // 0 is done by calling tryAcquire(0). Releasing access
@@ -9,6 +11,7 @@ public class Assignment4Rooms {
 
 	// Increast this constant if you need more controllers.
 	private boolean locked[] = new boolean[10];
+	private ArrayList<Long> indoorPermission = new ArrayList<Long>();
 	
 	public boolean tryAcquire(long n) {
 		int m = (int) n - 1;
@@ -23,5 +26,20 @@ public class Assignment4Rooms {
 	public void release(long n) {
 		int m = (int) n - 1;
 		locked[m] = false;
+	}
+	
+	public boolean tryIndoorPermission (long n) {
+		if (indoorPermission.size() >= 3) {
+			return false;			
+		} else if (indoorPermission.contains(n)) {
+			return true;			
+		} else {
+			indoorPermission.add(n);
+			return true;
+		}
+	}
+	
+	public void releaseIndoorPermission(long n) {
+		indoorPermission.remove(n);
 	}
 }
